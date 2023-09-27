afc3c0660455f681070a82b8ead010fbc87e0dfa
mvalenzuela <alanmarcosvalenzuela@gmail.com>
2023-09-26 21:54:45 -0300

    feat: api_user API

--- backend/user_api/admin.py
+++ backend/user_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AppUser
 
-# Register your models here.
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'email', 'username', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'username')
+    ordering = ('user_id',)
+
+admin.site.register(AppUser, AppUserAdmin)
--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class AppUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Un email es requerido')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser debería tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser debería tener is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = AppUserManager()
+
+    def __str__(self):
+        return self.username
+
+    def groups(self):
+        pass
+
+    def user_permissions(self):
+        pass
