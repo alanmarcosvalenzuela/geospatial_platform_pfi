6ebb0d4a11acd536ca33acb0dacacd026a1a99ec
mvalenzuela <alanmarcosvalenzuela@gmail.com>
2023-09-26 22:04:09 -0300

    geoprocess API

--- backend/geoprocess/admin.py
+++ backend/geoprocess/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import GeoProcess, Report
 
-# Register your models here.
+admin.site.register(GeoProcess)
+admin.site.register(Report)
--- backend/geoprocess/urls.py
+++ backend/geoprocess/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('reports/<str:email>/', views.UserReportsAPIView.as_view(), name='reports'),
+    path('report-download/<int:report_id>/', views.ReportDownloadView.as_view(), name='report-download'),
+    path('geoprocess-status/<int:geoprocess_id>/', views.GeoProcessStatusAPIView.as_view(), name='geoprocess-status'),
+    path('process', views.Process.as_view(), name='process'),
 ]
