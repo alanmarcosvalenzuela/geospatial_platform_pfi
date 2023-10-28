ad84809b2e0afa8b095709d0d09946df069a6953
Walter Shilman <48442478+wshilman@users.noreply.github.com>
2023-10-28 20:17:03 -0300

    Add postgress db_connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'terradata',
+        'PASSWORD': 'terradata_default_password$',
+        'HOST': 'postgres_docker_db',
+        'PORT': '5432',
     }
 }
 
