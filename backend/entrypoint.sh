9d68a2134e077470b01075f1ed5ffc1028c84cd8
Walter Shilman <48442478+wshilman@users.noreply.github.com>
2023-10-29 15:41:47 -0300

    Fix: createuser entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
-python manage.py createsuperuser --username terradata --email terradatademo@gmail.com --password Terre839ouasM --noinput
+python DJANGO_SUPERUSER_USERNAME=terradata DJANGO_SUPERUSER_EMAIL=terradatademo@gmail.com DJANGO_SUPERUSER_PASSWORD=Terre839ouasM python manage.py createsuperuser --noinput
 python manage.py runserver 0.0.0.0:8000
