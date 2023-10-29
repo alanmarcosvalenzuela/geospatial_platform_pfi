532310be725a21aa6f9732804e9cffbbf046b07e
Walter Shilman <48442478+wshilman@users.noreply.github.com>
2023-10-29 15:23:03 -0300

    Update: now it uses an entrypoint to build de backend container

---
+++ backend/entrypoint.sh
@@ -0,0 +1,3 @@
+python manage.py migrate
+python manage.py createsuperuser --username terradata --email terradatademo@gmail.com --password Terre839ouasM --noinput
+python manage.py runserver 0.0.0.0:8000
